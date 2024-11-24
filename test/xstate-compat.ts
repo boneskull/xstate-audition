87bea88e5cdcda1e83f633713974770b6d8160ea
Christopher Hiller <boneskull@boneskull.com>
2024-11-24 14:47:51 -0800

    chore(ci): update compat test to fail if minimum supported version is out-of-range of peer dependencies

--- test/xstate-compat.ts
+++ test/xstate-compat.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env tsx
 /**
  * This script runs the test suite against all versions of `xstate` greater than
- * or equal to the {@link KNOWN_MINIMUM known minimum version}.
+ * or equal to the {@link knownMinimum known minimum version}.
  *
  * This version should be the base for the `peerDependencies.xstate` range in
  * `package.json`.
@@ -15,92 +15,108 @@
  * @packageDocumentation
  */
 
-import {minVersion, parse, Range} from 'semver';
+import {minVersion, parse, Range, satisfies} from 'semver';
 import {$, type ProcessOutput} from 'zx';
 
-import {peerDependencies} from '../package.json' with {type: 'json'};
+import pkg from '../package.json' with {type: 'json'};
 
-/**
- * The known minimum version of `xstate` that works with `xstate-audition`.
- */
-const KNOWN_MINIMUM = minVersion(new Range(peerDependencies.xstate))!.format();
+const {peerDependencies} = pkg;
 
-/**
- * All versions of `xstate` available on npm
- */
-const allVersions = await $`npm show xstate versions --json`.json<string[]>();
+const main = async (): Promise<void> => {
+  /**
+   * The range of `xstate` versions that `xstate-audition` is compatible with.
+   */
+  const range = new Range(peerDependencies.xstate);
 
-/**
- * All versions newer than the known minimum (inclusive)
- *
- * Versions must be `SemVer` versions and not naughty things instead
- */
-const versionsUnderTest = allVersions
-  .slice(allVersions.indexOf(KNOWN_MINIMUM))
-  .filter((version) => parse(version) !== null);
+  /**
+   * The known minimum version of `xstate` that works with `xstate-audition`.
+   */
+  const knownMinimum = minVersion(range)!.format();
 
-/**
- * The minimum version that passed the test suite
- */
-let foundMinimum: string = '';
+  /**
+   * All versions of `xstate` available on npm
+   */
+  const allVersions = await $`npm show xstate versions --json`.json<string[]>();
 
-// We abort this signal upon SIGINT
-const ac = new AbortController();
+  /**
+   * All versions newer than the known minimum (inclusive)
+   *
+   * Versions must be `SemVer` versions and not naughty things instead
+   */
+  const versionsUnderTest = allVersions
+    .slice(allVersions.indexOf(knownMinimum))
+    .filter((version) => parse(version) !== null);
 
-const {signal} = ac;
+  /**
+   * The minimum version that passed the test suite
+   */
+  let foundMinimum: string = '';
 
-process
-  // if we got CTRL-C, then we need to restore the xstate version
-  .once('SIGINT', (signal) => {
-    ac.abort();
-    process.stderr.write('\nAborted; restoring xstate version …');
-    void $.sync({quiet: true})`npm install --force`;
-    process.stderr.write(' done\n');
-    process.emit('SIGINT', signal);
-  })
-  // and we need to restore it before exit as well
-  .once('beforeExit', () => {
-    process.stderr.write('\nRestoring xstate version …');
-    void $.sync({quiet: true})`npm install --force`;
-    process.stderr.write(' done\n');
-  });
+  // We abort this signal upon SIGINT
+  const ac = new AbortController();
 
-type Failure = {
-  error: ProcessOutput;
-  version: string;
-};
+  const {signal} = ac;
 
-const unexpectedFailures: Failure[] = [];
+  if (!process.env.CI) {
+    process
+      // if we got CTRL-C, then we need to restore the xstate version
+      .once('SIGINT', (signal) => {
+        ac.abort();
+        process.stderr.write('\nAborted; restoring xstate version …');
+        void $.sync({quiet: true})`npm install --force`;
+        process.stderr.write(' done\n');
+        process.emit('SIGINT', signal);
+      })
+      // and we need to restore it before exit as well
+      .once('beforeExit', () => {
+        process.stderr.write('\nRestoring xstate version …');
+        void $.sync({quiet: true})`npm install --force`;
+        process.stderr.write(' done\n');
+      });
+  } else {
+    process.once('beforeExit', () => {
+      console.error('CI detected; skipping version restoration');
+    });
+  }
 
-// unfortunately this must run in serial
-for (const version of versionsUnderTest) {
+  type Failure = {
+    error: ProcessOutput;
+    version: string;
+  };
+
+  const unexpectedFailures: Failure[] = [];
+
+  // unfortunately this must run in serial
+  for (const version of versionsUnderTest) {
+    try {
+      await $({quiet: true, signal})`npm i xstate@${version} --no-save`;
+    } catch (err) {
+      if (signal.aborted) {
+        continue;
+      }
+      console.error(`xstate@${version} - uninstallable`, err);
+      continue;
+    }
+    try {
+      process.stderr.write(`xstate@${version} …`);
+      await $({quiet: true, signal})`npm run build && npm test`;
+      foundMinimum ||= version;
+      process.stderr.write(` OK\n`);
+    } catch (err) {
+      if (signal.aborted) {
+        continue;
+      }
+      process.stderr.write(` NOT OK\n`);
+      unexpectedFailures.push({error: err as ProcessOutput, version});
+    }
+  }
+
   if (signal.aborted) {
-    continue;
-  }
-  try {
-    await $({quiet: true, signal})`npm i xstate@${version} --no-save`;
-  } catch (err) {
-    if (signal.aborted) {
-      continue;
-    }
-    console.error(`xstate@${version} - uninstallable`, err);
-    continue;
-  }
-  try {
-    process.stderr.write(`xstate@${version} …`);
-    await $({quiet: true, signal})`npm run build && npm test`;
-    foundMinimum ||= version;
-    process.stderr.write(` OK\n`);
-  } catch (err) {
-    if (signal.aborted) {
-      continue;
-    }
-    process.stderr.write(` NOT OK\n`);
-    unexpectedFailures.push({error: err as ProcessOutput, version});
-  }
-}
+    process.exitCode = 1;
 
-if (!signal.aborted) {
+    return;
+  }
+
   if (unexpectedFailures.length) {
     console.error('Unexpected failures:');
     for (const {error, version} of unexpectedFailures) {
@@ -108,11 +124,22 @@ if (!signal.aborted) {
       console.error(error);
     }
     process.exitCode = 1;
+
+    return;
   }
+
   if (foundMinimum) {
-    console.log('Minimum compatible version:', foundMinimum);
-  } else {
-    console.error('No passing versions found!');
-    process.exitCode = 1;
+    console.error('Minimum compatible version:', foundMinimum);
+    if (!satisfies(foundMinimum, range)) {
+      console.error('Minimum version does not satisfy range:', range);
+      process.exitCode = 1;
+    }
+
+    return;
   }
-}
+
+  console.error('No passing versions found!');
+  process.exitCode = 1;
+};
+
+void main();
